32cbc5b906425f1dc4f6da4c439aabcf3e00eda9
alaamahasneh <alaa.ibraheem97@yahoo.com>
2019-12-16 21:25:06 +0200

    wholelab

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # lab02
+Hello!
+our website here about :
+that you can choose one keyword of image from drowpdown menu and it will show to you all images of the same keywords.
+i worked in pair with ashar ^_^
+
+
+
+
+
+
+Number and name of feature: ____to filter keyword____________________________
+
+Estimate of time needed to complete: __30min___
+
+Start time: 8:00pm_____
+
+Finish time: _9:00pm____
+
+Actual time needed to complete: _1hour____
---
+++ js/app.js
@@ -0,0 +1,43 @@
+'use strict'
+function Image(image_url, title, description, keyword, horns) {
+    this.image_url = image_url;
+    this.title = title;
+    this.description = description;
+    this.keyword = keyword;
+    this.horns = horns;
+}
+let keywords = [];
+Image.prototype.render = function () {
+    let showType = $('<li><h2></h2><img/><p></p></li>').clone();
+    showType.find('h2').text(this.title);
+    showType.find('img').attr('src', this.image_url);
+    showType.find('p').text(this.description);
+    $('#photo-template').append(showType);
+}
+Image.prototype.filterKeword = function () {
+    // google ( how to check if something is already in array js)
+    //2 try to find if keyword is in the array "keywords"
+    //3 if not we should push (keywords will have unique keywords)
+    if (!(keywords.includes(this.keyword))) {
+        //4 we need to put them in the select new function
+        keywords.push(this.keyword);
+        // 5 loop through the keywords append to dropdown  
+        $("#dropdown1").append("<option >" + this.keyword + "</option>");
+        $( "option" ).click( function() {
+            $('li').hide();
+
+          $('#photo-template').html('<h1>'+ this.title+'</h1>')
+    });
+}
+}
+$.get('/data/page-1.json')
+    .then(data => {
+        data.forEach(element => {
+            let newImage = new Image(element.image_url, element.title, element.description, element.keyword, element.horns);
+            newImage.filterKeword();
+            newImage.render();
+        });
+    })
+
+
+
